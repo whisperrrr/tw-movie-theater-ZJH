4f0cd7aade6b29af5da3e85aacafbeb3168d5a59
JinHui <jh731448520@163.com>
2020-01-13 20:07:41 +0800

    [JinHui]{feat}-{NO1/2}:add function getMovieList() to get movie list

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 var MovieList; //前250电影列表，用于制作首页
 var MovieData; //单个电影数据，用于制作详情页
+var BASIC_URL = 'http://127.0.0.1:8888';
 window.onload = function () {
- // getMovieList();
+  getMovieList();
+  
   var movieId = '26942674';
   getMovieData(movieId);
 }
+function getMovieList() {
+  options = {
+    url: BASIC_URL + '/v2/movie/top250',
+    method: "GET",
+    data: {
+      apikey: '0df993c66c0c636e29ecbb5344252a4a'
+    },
+    success: function(data) {
+      console.log("get movie list success");
+      MovieList = data;
+    },
+    error: function(error) {
+      console.log("error",error);
+    }
+  }
+  ajax(options);
+}
 function getMovieData(movieId) {
-  var BASIC_URL = 'http://127.0.0.1:8888';
   var movieId = movieId;
 
   options = {
@@ -16,7 +34,7 @@ function getMovieData(movieId) {
       apikey: '0df993c66c0c636e29ecbb5344252a4a'
     },
     success: function(data) {
-      console.log("get movie data");
+      console.log("get movie data success");
       MovieData = data;
     },
     error: function(error) {
