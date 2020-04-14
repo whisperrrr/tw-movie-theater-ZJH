ec4a83f9421131d7cd5995e9936c8360ae147668
JinHui <jh731448520@163.com>
2020-04-15 01:24:52 +0800

    [JinHui]{fix}-{#A5,A6}:render data from database

--- js/detail-page.js
+++ js/detail-page.js
@@ -1,24 +1,62 @@
 let MovieList; //前250电影列表，用于制作首页
 let MovieData; //单个电影数据，用于制作详情页
+let CommentData; //单个电影的评论数据
+let ActorData;  //单个电影的演员数据 
 let classMovieList;  //通过类别筛选电影列表
 let movieListId; //所有电影的id编号集合
-let BASIC_URL = 'http://127.0.0.1:8888';
+//let BASIC_URL = 'http://127.0.0.1:8888';
+let BASIC_URL = 'http://localhost:8080';
 let apikeys= ['0df993c66c0c636e29ecbb5344252a4a','0b2bdeda43b5688921839c8ecb20399b'];
 let param = window.location.href.split("?")[1];
 let id = param.split("=")[1];
+getActorData(id);
+getCommentData(id);
 getMovieData(id);
   //通过豆瓣id获得相应电影详情
 function getMovieData(movieId) {
   options = {
-    url: BASIC_URL + '/v2/movie/subject/' + movieId,
+    url: BASIC_URL + '/movie/' + movieId,
     method: "GET",
     data: {
-      apikey: apikeys[0]
     },
     success: function(data) {
       console.log("get movie data success");
       MovieData = data;
-      renderDetailPageInfo(data);
+      renderDetailPageInfo();
+    },
+    error: function(error) {
+      console.log("error",error);
+    }
+  }
+  ajax(options);
+}
+
+function getActorData(movieId) {
+  options = {
+    url: BASIC_URL + '/movie/actor/' + movieId,
+    method: "GET",
+    data: {
+    },
+    success: function(data) {
+      console.log("get actor data success");
+      ActorData = data;
+    },
+    error: function(error) {
+      console.log("error",error);
+    }
+  }
+  ajax(options);
+}
+
+function getCommentData(movieId) {
+  options = {
+    url: BASIC_URL + '/movie/comment/' + movieId,
+    method: "GET",
+    data: {
+    },
+    success: function(data) {
+      console.log("get comment data success");
+      CommentData = data;
     },
     error: function(error) {
       console.log("error",error);
@@ -28,21 +66,21 @@ function getMovieData(movieId) {
 }
 
 //渲染详情页详细信息
-function renderDetailPageInfo(data) {
+function renderDetailPageInfo() {
   console.log("renderDetailPageInfo")
-  renderDetailPageTitle(data);
-  renderDetailPageInfos(data);
-  renderDetailPageReview(data);
-  renderDetailPageCommits(data);
-  renderDetailPageRecommand(data);
+  renderDetailPageTitle();
+  renderDetailPageInfos();
+  renderDetailPageReview();
+  renderDetailPageCommits();
+  renderDetailPageRecommand();
 }
 
 //渲染标题
-function renderDetailPageTitle(data) {
+function renderDetailPageTitle() {
   let title = document.getElementById("pop-movie-title");
-  title.innerHTML = `
-  <h2>${data.title}</h2>
-  <span>${data.original_title}</span>
+    title.innerHTML = `
+  <h2>${MovieData.name}</h2>
+  <span>${MovieData.originTitle}</span>
   `
 }
 
@@ -50,10 +88,10 @@ function renderDetailPageTitle(data) {
 function renderDetailPageInfos(data) {
   let movieInfo = document.getElementById("movie-info");
   let movieInfoData = movieInfo.querySelectorAll("span");
-  let dataArray = [data.title, data.genres,
-                   data.languages, data.pubdates,
-                   data.durations, data.rating.average,
-                   data.directors[0].name, getCastName(data.casts)];
+    let dataArray = [MovieData.name, MovieData.type,
+                   MovieData.languages, MovieData.year,
+                   MovieData.durations, MovieData.rating,
+                   MovieData.director, getCastName(ActorData)];
   for (let i = 0; i < movieInfoData.length; i++) {
     movieInfoData[i].innerHTML = dataArray[i];
   }
@@ -64,9 +102,9 @@ function renderDetailPageInfos(data) {
 function renderPoster(data) {
   let info = document.getElementById("pop-movie-info");
   let poster = info.querySelectorAll(".poster");
-  poster[0].innerHTML = `<img src=${data.images.small} alt="poster">`;
-  poster[1].innerHTML = `<img src=${data.casts[0].avatars.small} alt="poster" title=${data.casts[0].name}>`;
-  poster[2].innerHTML = `<img src=${data.casts[1].avatars.small} alt="poster" title=${data.casts[1].name}>`;
+  poster[0].innerHTML = `<img src=${MovieData.smallImg} alt="poster">`;
+  poster[1].innerHTML = `<img src=${ActorData[0].smallImg} alt="poster" title=${ActorData[0].name}>`;
+  poster[2].innerHTML = `<img src=${ActorData[1].smallImg} alt="poster" title=${ActorData[1].name}>`;
 }
 
 //获得演员名字
@@ -82,32 +120,33 @@ function getCastName(data) {
 function renderDetailPageReview(data) {
   let popMovieReview = document.getElementById("pop-movie-review");
   let popMovieReviewInfo = popMovieReview.querySelector("p");
-  popMovieReviewInfo.innerHTML = data.summary;
+  popMovieReviewInfo.innerHTML = MovieData.summary;
   console.log("render review success...");
 }
 
 //渲染评论
 function renderDetailPageCommits(data) {
   let popMovieCommit = document.getElementById("pop-movie-commits");
-  popMovieCommit.innerHTML = Array.from(data.popular_reviews).reduce((acc,cur)=> {
-    return acc += `<div class="commits">
-                      <strong>${cur.author.name}:</strong> <!--data.popular_reviews.author.name-->
-                      <span>${cur.summary}</span>
-                  </div>`
-    },'')
+  popMovieCommit.innerHTML = Array.from(CommentData).reduce((acc,cur)=> {
+  return acc += `<div class="commits">
+                    <strong>${cur.name}:</strong> <!--data.popular_reviews.author.name-->
+                    <span>${cur.commitInfo}</span>
+                </div>`
+  },'')
   console.log("render commits success...");
 }
 
 //渲染相似推荐
 function renderDetailPageRecommand(data) {
-  let gen = data.genres[1] || data.genres[0];
+  let movieTypeList = MovieData.type.split(",");
+  let gen = movieTypeList[1] || movieTypeList[0];
   initialDetailPageMovie(filterByClass(gen));
 }
 
 //通过类别筛选电影列表
 function filterByClass(classWanted) {
-  let movieListSubject = JSON.parse(localStorage.data).subjects;
-  movieListSubject = movieListSubject.filter(ele => ele.genres.indexOf(classWanted) > -1);
+  let movieListSubject = JSON.parse(localStorage.data);
+  movieListSubject = movieListSubject.filter(ele => ele.type.indexOf(classWanted) > -1);
   return movieListSubject;
 }
 
@@ -116,15 +155,15 @@ function initialDetailPageMovie(data) {
   let popMovieRecommand = document.getElementById("pop-movie-recommand");
   if (data.length) {
     popMovieRecommand.innerHTML = Array.from(data).reduce((acc, cur) => {
-      return acc += `<a target="_blank" href="./detail-page.html?id=${cur.id}">
-                      <div class="movie-info" movie-id=${cur.id}">
-                        <div class="poster"  movie-id=${cur.id} style="background-image:url(${cur.images.small})"></div>
-                        <div class="brief-info">
-                          <p class="movie-show-title" movie-id=${cur.id}>${cur.title}</p>
-                          <p class="summary">${cur.genres.join(' ')}</p>
-                        </div>
-                      </div>
-                     </a> `
+        return acc += `<a target="_blank" href="./detail-page.html?id=${cur.id}">
+        <div class="movie-info movie-id=${cur.id}">
+          <div class="poster"  movie-id=${cur.id} style="background-image:url(${cur.smallImg})"></div>
+          <div class="brief-info">
+            <p class="movie-show-title" movie-id=${cur.id}>${cur.name}</p>
+            <p class="summary">${cur.type}</p>
+          </div>
+        </div>
+      </a> `
     }, "")
   } else {
     popMovieRecommand.innerHTML = `<strong class="no-result">没有搜索到结果</strong>`
